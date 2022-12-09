b23cc6a3cc4942e718cf7747c3c9d0d09fe850ba
furkancalikk7 <furkan.calik@ug.bilkent.edu.tr>
2022-12-10 00:30:13 +0300

    Thymeleaf is implemented

---
+++ src/main/java/com/database/eventmania/backend/configuration/DataSourceConfiguration.java
@@ -0,0 +1,8 @@
+package com.database.eventmania.backend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfiguration {
+
+}
--- src/main/java/com/database/eventmania/backend/controller/AdminController.java
+++ src/main/java/com/database/eventmania/backend/controller/AdminController.java
@@ -1,27 +1,30 @@
 package com.database.eventmania.backend.controller;
 
 import com.database.eventmania.backend.entity.Admin;
+import com.database.eventmania.backend.model.LoginModel;
 import com.database.eventmania.backend.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.sql.SQLException;
 
 @RestController
 @RequestMapping(path = "admin")
 public class AdminController {
-    private final AdminService adminService;
-
-    @Autowired
-    public AdminController(AdminService adminService) {
-        this.adminService = adminService;
+    @GetMapping("login")
+    public ModelAndView loginPage() {
+        ModelAndView mav = new ModelAndView("admin/login.html");
+        LoginModel loginModel = new LoginModel();
+        mav.addObject("login", loginModel);
+        // TODO: Change later
+        mav.addObject("access_token", "test");
+        return mav;
     }
 
-    @GetMapping(path = "getAdmin/{adminId}")
-    public Admin getAdminById(@PathVariable("adminId") Long adminId) throws SQLException {
-        return adminService.getAdminById(adminId);
+    @PostMapping()
+    public String login(@RequestParam(value = "access") String accessToken, @ModelAttribute("loginModal") LoginModel loginModel) {
+        // TODO: ADD LOGIN METHOD HERE
+        return "";
     }
 }
