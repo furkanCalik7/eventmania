84847e7c26082fb9ba1029309f26f0c60153d98c
nadirkaraman2001 <nadirkaraman2001@gmail.com>
2022-12-09 22:16:34 +0300

    Implements Location entity class.

---
+++ src/main/java/com/database/eventmania/backend/entity/Location.java
@@ -0,0 +1,22 @@
+package com.database.eventmania.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Location {
+    private Long eventId;
+    private String latitude;
+    private String longitude;
+    private String locationName;
+    private String addressDescription;
+    private String country;
+    private String postalCode;
+    private String state;
+    private String city;
+    private String street;
+
+}
