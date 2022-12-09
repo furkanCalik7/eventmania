742e4aa350eddbc0ef87497cf5f68e795e51d243
nadirkaraman2001 <nadirkaraman2001@gmail.com>
2022-12-09 18:24:04 +0300

    Changes LocalDate property in UnticketedEvent to LocalDateTime

--- src/main/java/com/database/eventmania/backend/entity/UnticketedEvent.java
+++ src/main/java/com/database/eventmania/backend/entity/UnticketedEvent.java
@@ -3,16 +3,17 @@ package com.database.eventmania.backend.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class UnticketedEvent extends Event {
     private Long userId;
     private Integer capacity;
 
-    public UnticketedEvent(Long eventId, Long adminId, String feedback, LocalDate verificationDate,
+    public UnticketedEvent(Long eventId, Long adminId, String feedback, LocalDateTime verificationDate,
                            VerificationStatus verificationStatus, String eventName, String eventDescription,
-                           LocalDate startDate, LocalDate endDate, Boolean isOnline, String imageUrl,
+                           LocalDateTime startDate, LocalDateTime endDate, Boolean isOnline, String imageUrl,
                            Integer minimumAge, EventState currentState, Long userId, Integer capacity) {
         super(eventId, adminId, feedback, verificationDate, verificationStatus, eventName, eventDescription, startDate,
                 endDate, isOnline, imageUrl, minimumAge, currentState);
