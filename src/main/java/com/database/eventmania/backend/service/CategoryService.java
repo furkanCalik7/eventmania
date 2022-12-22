073922c59738e22a12d74df92b704dcf739ab801
Berkay Çalmaz <berkay.calmaz@ug.bilkent.edu.tr>
2022-12-22 19:54:11 +0300

    Category creation implemented

---
+++ src/main/java/com/database/eventmania/backend/service/CategoryService.java
@@ -0,0 +1,22 @@
+package com.database.eventmania.backend.service;
+
+import com.database.eventmania.backend.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
+@Service
+public class CategoryService {
+    CategoryRepository categoryRepository;
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository){
+        this.categoryRepository = categoryRepository;
+    }
+
+    public boolean addCategory(String eventId, String categoryName, String categoryDescription, String capacity, String  price) throws SQLException {
+        return categoryRepository.addCategory(Long.valueOf(eventId), categoryName, categoryDescription, Integer.valueOf(capacity), Integer.valueOf(price));
+    }
+
+
+}
